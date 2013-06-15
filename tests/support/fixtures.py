befa1a0063506d08945c4446f74d965c105296d7
Nick Loadholtes <nick@ironboundsoftware.com>
2013-06-15 00:05:07 -0400

    Adding in the tests and support fixtures

---
+++ tests/support/fixtures.py
@@ -0,0 +1,15 @@
+import unittest
+
+class Tests(unittest.TestCase):
+
+    def test_A(self):
+        pass
+
+    def test_B(self):
+        pass
+
+    def test_C(self):
+        pass
+
+    def test_D(self):
+        pass
---
+++ tests/test_randomize.py
@@ -0,0 +1,33 @@
+"""
+Tests that plugins can override loadTestsFromTestCase
+"""
+import os
+import unittest
+from nose import loader
+from nose.plugins import PluginTester
+from nose.plugins.randomize import Randomize
+from nose.plugins.base import Plugin
+
+support = os.path.join(os.path.dirname(__file__), 'support')
+
+class TestRandomizePlugin(PluginTester, unittest.TestCase):
+
+    activate = '--randomize'
+    args = ['-v', '--seed=2530711073']
+    plugins = [Randomize()]
+    suitepath = os.path.join(support, 'randomize')
+
+    def runTest(self):
+        expect = [
+            'test_C (tests.Tests) ... ok',
+            'test_B (tests.Tests) ... ok',
+            'test_A (tests.Tests) ... ok',
+            'test_D (tests.Tests) ... ok']
+        print str(self.output)
+        for line in self.output:
+            if expect:
+                self.assertEqual(line.strip(), expect.pop(0))
+                
+
+if __name__ == '__main__':
+    unittest.main()
