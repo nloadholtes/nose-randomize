90827e707b23ec5c1453de1dd7da802c3c1036f0
Nick Loadholtes <nick@ironboundsoftware.com>
2013-06-17 21:53:00 -0400

    Adding in a fixture to allow multiple test classes to be tested for randomization

---
+++ tests/support/fixtures_two_classes.py
@@ -0,0 +1,31 @@
+"""
+Fixture to test multiple test classes
+"""
+import unittest
+
+class Tests_A_set(unittest.TestCase):
+
+    def test_A_1(self):
+        pass
+
+    def test_A_2(self):
+        pass
+
+    def test_A_3(self):
+        pass
+
+    def test_A_4(self):
+        pass
+
+class Tests_B_set(unittest.TestCase)
+    def test_B_1(self):
+        pass
+
+    def test_B_2(self):
+        pass
+
+    def test_B_3(self):
+        pass
+
+    def test_B_4(self):
+        pass
