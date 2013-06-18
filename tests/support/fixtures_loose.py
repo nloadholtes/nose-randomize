0e8846644b58eb57717528622be6e1fe75339190
Nick Loadholtes <nick@ironboundsoftware.com>
2013-06-18 10:09:41 -0400

    Adding test to look at 'loose' test functions.

---
+++ tests/support/fixtures_loose.py
@@ -0,0 +1,15 @@
+"""
+'loose' test functions
+"""
+
+def test_loose_A(self):
+    pass
+
+def test_loose_B(self):
+    pass
+
+def test_loose_C(self):
+    pass
+
+def test_loose_D(self):
+    pass
--- tests/test_randomize.py
+++ tests/test_randomize.py
@@ -50,5 +50,24 @@ class TestRandomizePluginMultipleTestClasses(PluginTester, unittest.TestCase):
             if expect:
                 self.assertEqual(line.strip(), expect.pop(0))
 
+
+class TestRandomizePluginLooseTestFunctions(PluginTester, unittest.TestCase):
+    activate = '--randomize'
+    args = ['-v', '--seed=1234112']
+    plugins = [Randomize()]
+    suitepath = os.path.join(support, 'fixtures_loose.py')
+
+    def runTest(self):
+        expect = [
+            'test_C (fixtures.Tests) ... ok',
+            'test_B (fixtures.Tests) ... ok',
+            'test_A (fixtures.Tests) ... ok',
+            'test_D (fixtures.Tests) ... ok']
+        print str(self.output)
+        for line in self.output:
+            if expect:
+                self.assertEqual(line.strip(), expect.pop(0))
+
+
 if __name__ == '__main__':
     unittest.main()
