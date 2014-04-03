60c3bfeadb2634d26314268772f8e2cbc7123bc4
Nick Loadholtes <nick@ironboundsoftware.com>
2014-04-02 22:09:16 -0400

    Adding a new test fixture to test the stop-on-dash-x flag reported in Issue #2

---
+++ tests/support/fixtures_loose_one_failure.py
@@ -0,0 +1,19 @@
+"""
+'loose' test functions, with A failing
+"""
+
+
+def test_loose_A():
+    assert 1 == 2
+
+
+def test_loose_B():
+    pass
+
+
+def test_loose_C():
+    pass
+
+
+def test_loose_D():
+    pass
