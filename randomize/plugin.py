98ebc274b04f21c80f597b13ed9e630b11ec4a30
Nick Loadholtes <nick@ironboundsoftware.com>
2014-04-18 17:15:09 -0400

    Adding in code from the collect sample code in nose.

--- randomize/plugin.py
+++ randomize/plugin.py
@@ -14,7 +14,7 @@ import logging
 from nose.plugins import Plugin
 from nose import loader
 from inspect import isfunction, ismethod
-from nose.case import FunctionTestCase, MethodTestCase
+from nose.case import FunctionTestCase, MethodTestCase, Test
 from nose.failure import Failure
 from nose.util import isclass, isgenerator, transplant_func, transplant_class
 import random
@@ -117,3 +117,60 @@ class Randomize(Plugin):
         random.shuffle(randomized_tests)
         tests._tests = (t for t in randomized_tests)
         return tests
+
+    def prepareTestLoader(self, loader):
+        """Install collect-only suite class in TestLoader.
+        """
+        # Disable context awareness
+        log.debug("Preparing test loader")
+        loader.suiteClass = TestSuiteFactory(self.conf)
+
+    def prepareTestCase(self, test):
+        """Replace actual test with dummy that always passes.
+        """
+        # Return something that always passes
+        log.debug("Preparing test case %s", test)
+        if not isinstance(test, Test):
+            return
+        def run(result):
+            # We need to make these plugin calls because there won't be
+            # a result proxy, due to using a stripped-down test suite
+            self.conf.plugins.startTest(test)
+            result.startTest(test)
+            self.conf.plugins.addSuccess(test)
+            result.addSuccess(test)
+            self.conf.plugins.stopTest(test)
+            result.stopTest(test)
+        return run
+
+
+class TestSuiteFactory:
+    """
+    Factory for producing configured test suites.
+    """
+    def __init__(self, conf):
+        self.conf = conf
+
+    def __call__(self, tests=(), **kw):
+        return TestSuite(tests, conf=self.conf)
+
+
+class TestSuite(unittest.TestSuite):
+    """
+    Basic test suite that bypasses most proxy and plugin calls, but does
+    wrap tests in a nose.case.Test so prepareTestCase will be called.
+    """
+    def __init__(self, tests=(), conf=None):
+        self.conf = conf
+        # Exec lazy suites: makes discovery depth-first
+        if callable(tests):
+            tests = tests()
+        log.debug("TestSuite(%r)", tests)
+        unittest.TestSuite.__init__(self, tests)
+
+    def addTest(self, test):
+        log.debug("Add test %s", test)
+        if isinstance(test, unittest.TestSuite):
+            self._tests.append(test)
+        else:
+            self._tests.append(Test(test, config=self.conf))
